97dba971ecf8ddc687bdf751d4f242548c2a49dc
Ilia Pachulia <ipachulia@evolution.com>
2022-09-01 13:41:52 +0400

    formBuilder was added

--- personal-project-angular/src/app/modules/registration/registration.module.ts
+++ personal-project-angular/src/app/modules/registration/registration.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration/registration.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
--- personal-project-angular/src/app/modules/registration/registration/registration.component.ts
+++ personal-project-angular/src/app/modules/registration/registration/registration.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,17 +13,48 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-
   file: any;
   clicked: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
 
+  form = this.formBuilder.group({
+    firstname: ['', Validators.required, Validators.minLength(2)],
+    lastname: ['', Validators.required, Validators.minLength(2)],
+    team: ['', Validators.required],
+    position: ['', Validators.required],
+    email: [
+      '',
+      Validators.required,
+      Validators.pattern('/^[A-Za-z0-9._%+-]+@redberry.ge$/'),
+    ], // should be end on @redberry.ge needs regex
+    phoneNumber: [
+      '',
+      Validators.required,
+      // Validators.pattern('/^(+?995)?(79d{7}|5d{8})$'),
+    ], // უნდა აკმაყოფილებდეს ქართული მობილური ნომრის ფორმატს რეჯექსით
+
+    photo: ['', Validators.required],
+
+    laptopName: [
+      '',
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9?><;,{}[]-!@#$%^&*()_+='),
+    ],
+    brand: ['', Validators.required],
+    laptopCPU: ['', Validators.required],
+    coreCPU: ['', Validators.required, Validators.pattern('^[0-9]')],
+    flowCPU: ['', Validators.required, Validators.pattern('^[0-9]')],
+    ram: ['', Validators.required, Validators.pattern('^[0-9]')],
+    storage: ['', Validators.required],
+    newOrUsed: ['', Validators.required],
+    date: [''],
+    price: ['', Validators.required, Validators.pattern('^[0-9]')],
+  });
+
   onClick() {
-    // this.clickedOnEmployeeForm =
-    //   !this.clickedOnEmployeeForm && !this.clickedOnLaptopForm;
     return (this.clicked = true);
   }
 
