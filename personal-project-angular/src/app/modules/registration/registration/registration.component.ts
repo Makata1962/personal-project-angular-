0ffcf731b0d112b31dbbcfa0f887ba506bcced91
Ilia Pachulia <ipachulia@evolution.com>
2022-09-02 18:39:43 +0400

    get and posts services are added and used in forms

--- personal-project-angular/src/app/modules/registration/registration/registration.component.ts
+++ personal-project-angular/src/app/modules/registration/registration/registration.component.ts
@@ -9,6 +9,49 @@ import {
 } from '@angular/forms';
 
 import { NgxFileDropEntry } from 'ngx-file-drop';
+import { ApiService } from 'src/app/services/api.service';
+import { subscribeOn } from 'rxjs';
+
+interface Team {
+  id: number;
+  name: string;
+}
+
+interface Positions {
+  id: number;
+  name: string;
+  teams_id: number;
+}
+export interface Brands {
+  id: number;
+  name: string;
+}
+
+interface Cpus {
+  id: number;
+  name: string;
+}
+
+export interface PostLaptop {
+  name: string;
+  surname: string;
+  team_id: number;
+  position_id: number;
+  phone_number: string;
+  email: string;
+  token: string;
+  laptop_name: string;
+  laptop_image: string;
+  laptop_brand_id: number;
+  laptop_cpu: string;
+  laptop_cpu_cores: number;
+  laptop_cpu_threads: number;
+  laptop_ram: number;
+  laptop_hard_drive_type: string;
+  laptop_state: string;
+  laptop_purchase_date?: string;
+  laptop_price: number;
+}
 
 @Component({
   selector: 'app-registration',
@@ -16,55 +59,109 @@ import { NgxFileDropEntry } from 'ngx-file-drop';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  disabled: boolean = true;
   clicked: boolean = false;
   public files: NgxFileDropEntry[] = [];
-  team = ['დეველოპმენტი', 'HR', 'გაყიდვები', 'დიზაინი', 'მარკეტინგი'];
-  positions = ['დეველოპმენტი', 'HR', 'გაყიდვები', 'დიზაინი', 'მარკეტინგი'];
+  teams: Team[] = [];
+  positions: Positions[] = [];
+  brands: Brands[] = [];
+  cpus: Cpus[] = [];
+  token: string = '4479704264a9c309e38267981be57000';
 
-  constructor(private http: HttpClient, private formBuilder: FormBuilder) {}
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder,
+    private api: ApiService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.showTeams();
+    this.showPositions();
+    this.showBrands();
+    this.showCpus();
+  }
 
   form = this.formBuilder.group({
-    firstname: ['', Validators.required, Validators.minLength(2)],
-    lastname: ['', Validators.required, Validators.minLength(2)],
-    team: ['', Validators.required],
-    position: ['', Validators.required],
-    email: [
+    name: [''],
+    surname: [''],
+    team_id: [{ id: 0, name: '' }],
+    position_id: [{ id: 0, name: '' }],
+    email: [''], // should be end on @redberry.ge needs regex
+    phone_number: [
       '',
-      Validators.required,
-      Validators.pattern('/^[A-Za-z0-9._%+-]+@redberry.ge$/'),
-    ], // should be end on @redberry.ge needs regex
-    phoneNumber: [
-      '',
-      Validators.required,
       // Validators.pattern('/^(+?995)?(79d{7}|5d{8})$'),
     ], // უნდა აკმაყოფილებდეს ქართული მობილური ნომრის ფორმატს რეჯექსით
 
-    photo: ['', Validators.required],
+    laptop_image: [''],
 
-    laptopName: [
-      '',
-      Validators.required,
-      Validators.pattern('^[a-zA-Z0-9?><;,{}[]-!@#$%^&*()_+='),
-    ],
-    brand: ['', Validators.required],
-    laptopCPU: ['', Validators.required],
-    coreCPU: ['', Validators.required, Validators.pattern('^[0-9]')],
-    flowCPU: ['', Validators.required, Validators.pattern('^[0-9]')],
-    ram: ['', Validators.required, Validators.pattern('^[0-9]')],
-    storage: ['', Validators.required],
-    newOrUsed: ['', Validators.required],
-    date: [''],
-    price: ['', Validators.required, Validators.pattern('^[0-9]')],
+    laptop_name: [''],
+    laptop_brand_id: [{ id: 0, name: '' }],
+    laptop_cpu: [''],
+    laptop_cpu_cores: [0],
+    laptop_cpu_threads: [0],
+    laptop_ram: [0],
+    laptop_hard_drive_type: [''],
+    laptop_state: [''],
+    laptop_purchase_date: [''],
+    laptop_price: [0],
   });
 
   onClick() {
-    return (this.clicked = true);
+    return (this.clicked = !this.clicked);
+  }
+
+  showTeams() {
+    this.api.getTeams().subscribe({
+      next: (res) => {
+        this.teams = res.data;
+      },
+    });
+  }
+
+  showPositions() {
+    this.api.getPositions().subscribe({
+      next: (res) => {
+        this.positions = res.data;
+      },
+    });
+  }
+
+  showBrands() {
+    this.api.getBrands().subscribe({
+      next: (res) => {
+        this.brands = res.data;
+      },
+    });
+  }
+
+  showCpus() {
+    this.api.getCpus().subscribe({
+      next: (res) => {
+        this.cpus = res.data;
+      },
+    });
   }
 
   dropped(files: NgxFileDropEntry[]) {
     this.files = files;
     console.log(this.files);
+  }
+
+  fileUpload() {
+    const data = {
+      ...this.form.value,
+      token: this.token,
+      team_id: this.form.value.team_id?.id ?? 0,
+      position_id: this.form.value.position_id?.id ?? 0,
+      laptop_brand_id: this.form.value.laptop_brand_id?.id ?? 0,
+    } as PostLaptop;
+
+    this.api.postLaptop(data).subscribe({
+      next: (res) => {
+        alert('Success');
+        console.log(res);
+        // this.form.reset();
+      },
+    });
   }
 }
---
+++ personal-project-angular/src/app/services/api.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  Brands,
+  PostLaptop,
+} from '../modules/registration/registration/registration.component';
+
+interface BrandsRequest {
+  data: Brands[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  baseUrl = 'https://pcfy.redberryinternship.ge/api/';
+
+  constructor(private http: HttpClient) {}
+  getTeams() {
+    return this.http.get<any>(this.baseUrl + 'teams');
+  }
+
+  getPositions() {
+    return this.http.get<any>(this.baseUrl + 'positions');
+  }
+
+  getBrands() {
+    return this.http.get<BrandsRequest>(this.baseUrl + 'brands');
+  }
+
+  getCpus() {
+    return this.http.get<any>(this.baseUrl + 'cpus');
+  }
+
+  postLaptop(data: PostLaptop) {
+    console.log('dataaa', data);
+    return this.http.post<PostLaptop>(this.baseUrl + 'laptop/create', data, {
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
